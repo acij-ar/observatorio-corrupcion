e055e7fff1f61109c143162cbfdf3a17e13cfb69
fnbellomo <fnbellomo@gmail.com>
2021-09-11 22:22:07 -0300

    initial commit

---
+++ scraper/index.js
@@ -0,0 +1,77 @@
+import ClusterModule  from 'puppeteer-cluster'
+
+import writer from './src/writer.js'
+
+const { Cluster } = ClusterModule
+const baseUrl = 'https://www.cij.gob.ar/causas-de-corrupcion.html'
+
+async function main() {
+  const cluster = await Cluster.launch({
+    concurrency: Cluster.CONCURRENCY_CONTEXT,
+    maxConcurrency: 5,
+    // monitor: true,
+    puppeteerOptions: {
+      headless: true,
+      args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+      ],
+    },
+  })
+
+  // Event handler to be called in case of problems
+  cluster.on('taskerror', (err, data) => {
+    console.log(`Error crawling ${data}: ${err.message}`)
+  })
+
+  await cluster.task(async ({ page, data: { total, paged, section } }) => {
+    console.log(`Scrapeando página ${paged}/${total} de la sección ${section}`)
+
+    await page.goto(baseUrl)
+    await page.evaluate(`irPaginaS${section}(${paged})`)
+    await page.waitForNavigation()
+    await page.addScriptTag({ path: 'src/utils.js' })
+
+    const rows = await page.evaluate(() =>
+      getNodesByXPath('//*[@id="solapa-1"]/div/ul[@class="info"]')
+        .map(ul => getNodesByXPath('li', ul))
+        .map(lis => parse(lis))
+    )
+
+    // Save to csv
+    rows.forEach(row => {
+      writer({ terminado: section === 1, ...row })
+    })
+  })
+
+  // Get the number of cases completed and pending
+  const [section1, section2] = await cluster.execute(
+    baseUrl,
+    async ({ page, data: url }) => {
+      await page.goto(url)
+      await page.addScriptTag({ path: 'src/utils.js' })
+      return await page.evaluate(() =>
+        getNodesByXPath('//*[@id="paginado-sup-s2"]/div[1]/span')
+          .map(r => +r.textContent)
+      )
+    }
+  )
+  console.log(`Hay ${section1} casusas de la sección 1 y ${section2} de la 2`)
+
+  // const section1Pages = parseInt(section1/6)
+  // const section2Pages = parseInt(section2/6)
+  // for (let i=0; i<section1Pages; i++) {
+  //   cluster.queue({ total: section1Pages, paged: i, section: 1 })
+  // }
+  // for (let i=0; i<section2Pages; i++) {
+  //   cluster.queue({ total: section2Pages, paged: i, section: 2 })
+  // }
+  cluster.queue({ total: 10, paged: 0, section: 1 })
+  cluster.queue({ total: 10, paged: 1, section: 1 })
+  cluster.queue({ total: 10, paged: 2, section: 1 })
+
+  await cluster.idle()
+  await cluster.close()
+}
+
+main();
---
+++ scraper/src/writer.js
@@ -0,0 +1,67 @@
+import fs from 'fs'
+
+const clear_line = (line) => {
+  return line
+    .map(val => {
+      try {
+        return val
+          .replace(/"/g, ' ')
+          .replace(/\n/g, ' ')
+      } catch (error) {
+        return val
+      }
+    })
+    .join('","')
+}
+
+const streams = {}
+const outputDir = 'data'
+const keys = [
+  'causas',
+  'delitos',
+  'radicacioon_del_expediente',
+  'resoluciones',
+  'implicados',
+]
+
+if (!fs.existsSync(outputDir)) {
+  fs.mkdirSync(outputDir)
+}
+
+keys.forEach(key => {
+  streams[key] = fs.createWriteStream(
+    `${outputDir}/${key}.csv`,
+    { flags: 'a' }
+  )
+})
+
+const write = (entry) => {
+  const lines = {}
+  entry.causas = [{
+    estado: entry.estado,
+    ultima_actualizacion: entry.ultima_actualizacion,
+    caratula: entry.caratula,
+    terminado: entry.terminado,
+  }]
+
+  keys.forEach(key => {
+    if (key in entry) {
+      lines[key] = entry[key]
+        .map(e => ({ expediente: entry.expediente, ...e }))
+    }
+  })
+
+  Object.keys(lines).forEach(key => {
+    lines[key].forEach(line => {
+      if (fs.readFileSync(`${outputDir}/${key}.csv`).length === 0) {
+        const header = clear_line(Object.keys(line))
+        streams[key].write(`"${header}"\n`)
+      }
+
+      line = clear_line(Object.values(line))
+      streams[key].write(`"${line}"\n`)
+    })
+  })
+}
+
+export default write
