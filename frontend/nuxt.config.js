3012f709fd3d93023f2d0f7b32a76a15e32b006c
fnbellomo <fnbellomo@gmail.com>
2021-11-29 12:34:19 -0300

    Setup vue-portal

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -40,7 +40,8 @@ module.exports = {
   ** Plugins to load before mounting the App
   */
   plugins: [
-    '~/plugins/font-awesome'
+    '~/plugins/font-awesome',
+    '~/plugins/portal',
   ],
 
   /*
