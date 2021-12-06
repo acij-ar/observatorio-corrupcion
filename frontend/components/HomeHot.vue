003907db602633c7d0a8b40d72278f3b54b0ac29
fnbellomo <fnbellomo@gmail.com>
2021-12-06 09:32:13 -0300

    Change who sort index entities

--- frontend/components/HomeHot.vue
+++ frontend/components/HomeHot.vue
@@ -5,46 +5,47 @@
       <h2 class="title is-2">Personas destacadas</h2>
 
       <div class="columns">
-        <card-entity v-if="entities[0] !== undefined" :name="entities[0]"></card-entity>
-        <card-entity v-if="entities[1] !== undefined" :name="entities[1]"></card-entity>
-        <card-entity v-if="entities[2] !== undefined" :name="entities[2]"></card-entity>
-        <card-entity v-if="entities[3] !== undefined" :name="entities[3]"></card-entity>
+        <card-entity
+          v-for="entitieName in entities"
+          :key="entitieName"
+          :name="entitieName"
+        />
       </div>
-    </div><!--/container -->
+    </div>
 
     <!-- Causas -->
     <div v-if="cases.length > 0" class="container">
       <h2 class="title is-2">Causas relevantes</h2>
 
       <div class="columns">
-        <card-case v-if="cases[0] !== undefined" :name="cases[0]"></card-case>
-        <card-case v-if="cases[1] !== undefined" :name="cases[1]"></card-case>
-        <card-case v-if="cases[2] !== undefined" :name="cases[2]"></card-case>
-        <card-case v-if="cases[3] !== undefined" :name="cases[3]"></card-case>
+        <card-case
+          v-for="caseName in cases"
+          :key="caseName"
+          :name="caseName"
+        />
       </div>
-    </div><!--/container -->
+    </div>
 
     <!-- Magistrados -->
     <div v-if="magistrates.length > 0" class="container">
-    <!-- <div v-if="false" class="container"> -->
       <h2 class="title is-2">Juezas/ces y Fiscales</h2>
 
       <div class="columns">
-        <card-magistrate v-if="magistrates[0] !== undefined" :name="magistrates[0]"></card-magistrate>
-        <card-magistrate v-if="magistrates[1] !== undefined" :name="magistrates[1]"></card-magistrate>
-        <card-magistrate v-if="magistrates[2] !== undefined" :name="magistrates[2]"></card-magistrate>
-        <card-magistrate v-if="magistrates[3] !== undefined" :name="magistrates[3]"></card-magistrate>
+        <card-magistrate
+          v-for="magistrateName in magistrates"
+          :key="magistrateName"
+          :name="magistrateName"
+        />
       </div>
-    </div><!--/container -->
+    </div>
   </section>
 </template>
 
 <script>
 import * as d3 from 'd3'
-import axios from 'axios'
-import CardEntity from '~/components/HomeHotCardEntity'
-import CardCase from '~/components/HomeHotCardCase'
-import CardMagistrate from '~/components/HomeHotCardMagistrate'
+import CardCase from '@/components/HomeHotCardCase'
+import CardEntity from '@/components/HomeHotCardEntity'
+import CardMagistrate from '@/components/HomeHotCardMagistrate'
 
 export default {
   data () {
@@ -55,99 +56,54 @@ export default {
     }
   },
   components: {
-    CardEntity,
     CardCase,
+    CardEntity,
     CardMagistrate
   },
   mounted () {
-    this.getHotEntities()
     this.getHotCases()
+    this.getHotEntities()
     this.getHotMagistrates()
   },
   methods: {
-    getHotEntities: function () {
-      /*
-      Descargo el csv con las entidades "hot" para poner en el home.
-      Las entidades del grupo 0 van a estar siempre. Los demas grupos los sorteo.
-      */
-      let url = 'https://docs.google.com/spreadsheets/d/17pOcSqgidnPtQudN8y6Cog3QA6c_WoEEGFHv4oyV_bM/edit#gid=0'
-      axios.get(url, { crossdomain: true }).then(result => {          
-          let text = result.data
-          let data = text.split("DIVIDER")[1]          
-          let array = data.split("\n")
-          let dictionary = []
-          array.forEach(d => {
-              let e = d.split(",")
-              let grupo = e[0]
-              let entidad = e[1]                            
-              if (entidad !== undefined && grupo !== undefined) {               
-                return dictionary.push({"grupo":grupo,"entidad":entidad})
-              }
-          })
-          let groups = []                
-          dictionary.forEach(d => {
-              return groups.push(d.grupo) 
-          })            
-          console.log("dictionary",dictionary);    
-          let groupRan = groups[Math.floor(Math.random() * groups.length)]                
-          let filter_groups = ["0", groupRan]         
-          let numeroAleatorio = Math.floor(Math.random() * (3) ) + 1;
-          let resultado = []
-          resultado[0]=dictionary[0].entidad
-          resultado[1]=dictionary[1].entidad
-          resultado[2]=dictionary[numeroAleatorio+numeroAleatorio].entidad
-          resultado[3]=dictionary[numeroAleatorio+numeroAleatorio+1].entidad
-          this.entities = resultado                      
-      });
-    },
-    getHotCases: function () {
-      /*
-      Descargo el csv con las causas "hot" para poner en el home
-      */
+    shuffle(array) {
+      let currentIndex = array.length,  randomIndex
 
-      let url = 'https://docs.google.com/spreadsheets/d/1qP1B8ECwJX-DJb92fduiIUk3my7ks2TklgXnGsZ96Lo/edit#gid=0'
-      axios.get(url, { crossdomain: true }).then(result => {          
-          let text = result.data
-          let data = text.split("DIVIDER")[1]          
-          let array = data.split("\n")          
-          let dictionary = []
-          array.forEach(d => {
-              let e = d.split(",")
-              let grupo = e[0]
-              let expediente = e[1]
-              let nombre = e[2]
-              if (grupo !== undefined && 
-                  nombre !== undefined &&
-                  expediente !== undefined) return dictionary.push({"grupo":grupo,"expediente":expediente,"nombre":nombre})
-          })
-          let groups = []                
-          dictionary.forEach(d => {
-              return groups.push(d.grupo) 
-          })                
-          
-          let groupRandom = dictionary[Math.floor(Math.random() * dictionary.length)]['grupo']               
-          this.cases = dictionary.filter(d => d.grupo === groupRandom).map(d => d.expediente.replace('/', '-'))       
-                   
-      });
+      // While there remain elements to shuffle...
+      while (currentIndex != 0) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [
+          array[randomIndex], array[currentIndex]];
+      }
+
+      return array
     },
-    getHotMagistrates: function () {
-      let url = 'https://docs.google.com/spreadsheets/d/1tRkXHJfbTmlwwT--a1Gf-f86gcdKNN0gN0o--Xre4oc/edit#gid=0'
-      axios.get(url, { crossdomain: true }).then(result => {        
-        let text = result.data
-        let data = text.split("DIVIDER")[1]        
-        let array = data.split("\n")              
-        let dictionary = []
-        array.forEach(d => {
-            let e = d.split(",")
-            let grupo = e[0]
-            let entidad = e[1]
-            if (grupo !== undefined && 
-                entidad !== undefined) return dictionary.push({"grupo":grupo,"entidad":entidad})
-        })
-        let groupRandom = dictionary[Math.floor(Math.random() * dictionary.length)]['grupo']
-        this.magistrates = dictionary.filter(d => d.grupo === groupRandom).map(d => d.entidad)        
-      });
-    }
+    async getHotEntities() {
+      const data = await d3.csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vR2hLJlo2hO6s1mvmTkRFHRhrBNOGTMgmkT_NHDEBI8-raXSar7z3HKm5r1pd5XeM-3Ll_qQhXhUNfl/pub?gid=2116993857&single=true&output=csv')
+      this.entities = [
+        ...this.shuffle(data.filter(row => row.grupo === '1')).slice(0, 2),
+        ...this.shuffle(data.filter(row => row.grupo === '2')).slice(0, 2),
+      ].map(row => row.nombre)
+    },
+    async getHotCases() {
+      const data = await d3.csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vR2hLJlo2hO6s1mvmTkRFHRhrBNOGTMgmkT_NHDEBI8-raXSar7z3HKm5r1pd5XeM-3Ll_qQhXhUNfl/pub?gid=2096219490&single=true&output=csv')
+      this.cases = [
+        ...this.shuffle(data.filter(row => row.grupo === '1')).slice(0, 2),
+        ...this.shuffle(data.filter(row => row.grupo === '2')).slice(0, 2),
+      ].map(row => row.expediente.replace('/', '-'))
+    },
+    async getHotMagistrates() {
+      const data = await d3.csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vR2hLJlo2hO6s1mvmTkRFHRhrBNOGTMgmkT_NHDEBI8-raXSar7z3HKm5r1pd5XeM-3Ll_qQhXhUNfl/pub?gid=0&single=true&output=csv')
+      this.magistrates = [
+        ...this.shuffle(data.filter(row => row.grupo === '1')).slice(0, 2),
+        ...this.shuffle(data.filter(row => row.grupo === '2')).slice(0, 2),
+      ].map(row => row.nombre)
+    },
   }
 }
 </script>
