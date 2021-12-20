739c4db64d0e7d62549cb3f87ee8ce743472148a
fnbellomo <fnbellomo@gmail.com>
2021-12-19 23:24:31 -0300

    Add labels on bubble

--- frontend/components/Charts/Bubble.vue
+++ frontend/components/Charts/Bubble.vue
@@ -4,7 +4,6 @@
 
 <script>
 import * as d3 from 'd3'
-import { wrapText2 } from '@/assets/utils'
 
 export default {
   data () {
@@ -42,46 +41,73 @@ export default {
       .attr("height", this.height)
       .attr("viewBox", [0, 0, this.width, this.height])
       .attr("style", "max-width: 100%; height: auto; height: intrinsic;")
+      .attr("font-size", 10)
+      .attr("font-family", "sans-serif")
+      .attr("text-anchor", "middle")
 
     this.draw()
   },
   methods: {
-    draw: function () {
-      let pack = data => d3.pack()
-        .size([this.width, this.height])
-        .padding(3)
-        (d3.hierarchy({children: data})
-          .sum(d => d.value))
+    draw () {
+        const data = JSON.parse(JSON.stringify(this.data))
+        // Compute the values.
+        const D = d3.map(data, d => d);
+        const V = data.map(row => row.value);
+        const G = data.map(row => row.name)
+        const I = d3.range(V.length).filter(i => V[i] > 0);
 
-      const root = pack(this.data)
+        // Unique the groups.
+        const groups = I.map(i => G[i]);
 
-      const leaf = this.svg.selectAll('g')
-        .data(root.leaves())
-        .enter()
-        .append('g')
-        .attr('transform', d => `translate(${d.x + 1},${d.y + 1})`)
+        // Compute labels and titles.
+        const L = data.map(d => [...d.name.split(), d.value.toLocaleString("es")].join(" "))
+        const T = data.map(d => `${d.name}\n${d.value.toLocaleString("es")}`)
 
-      leaf.append('circle')
-        .attr('id', d => d.data.name.replace(/ /g, '_'))
-        .attr('r', d => d.r)
-        .attr('fill-opacity', 0.7)
-        .attr('fill', (d, i) => d3.schemeCategory10[i % 10])
+        // Compute layout: create a 1-deep hierarchy, and pack it.
+        const root = d3.pack()
+            .size([this.width, this.height])
+            .padding(3)
+          (d3.hierarchy({children: I})
+            .sum(i => V[i]))
 
-      leaf.append('clipPath')
-        .attr('id', d => 'clip-' + d.data.name.replace(/ /g, '_'))
-        .append('use')
-        .attr('xlink:href', d => '#' + d.data.name.replace(/ /g, '_'))
+        let leaf = this.svg.selectAll("a")
+          .data(root.leaves())
 
-      leaf.append('text')
-        .attr('clip-path', d => 'url(#clip-' + d.data.name.replace(/ /g, '_') + ')')
-        // .attr('x', d => -(d.r / 2))
-        // .attr('y', d => -(d.r / 2))
-        .text(d => `${d.value}`)
-        // .text(d => `${d.value} ${d.data.name}`)
-        // .call(wrapText2)
+        leaf.exit().remove()
 
-      leaf.append('title')
-        .text(d => d.data.name.concat([`\nCantidad: ${d.value}`]))
+        let leafEnter = leaf.enter()
+          .append('a')
+          .attr("transform", d => `translate(${d.x},${d.y})`)
+
+        leafEnter.append("circle")
+          .attr("fill", (d, i) => d3.schemeCategory10[i % 10])
+          .attr("fill-opacity", 0.7)
+          .attr("r", d => d.r)
+
+        leafEnter.append("title")
+          .text(d => T[d.data])
+
+        // A unique identifier for clip paths (to avoid conflicts).
+        const uid = `O-${Math.random().toString(16).slice(2)}`;
+
+        leafEnter.append("clipPath")
+            .attr("id", d => `${uid}-clip-${d.data}`)
+          .append("circle")
+            .attr("r", d => d.r);
+
+        const tspan = leafEnter.append("text")
+          .attr("clip-path", d => `url(${new URL(`#${uid}-clip-${d.data}`, location)})`)
+          .selectAll("tspan")
+          .data(d => `${L[d.data]}`.split(/ /g))
+
+        tspan
+          .enter()
+          .append('tspan')
+          .merge(tspan)
+            .attr("x", 0)
+            .attr("y", (d, i, D) => `${i - D.length / 2 + 0.85}em`)
+            .attr("fill-opacity", (d, i, D) => i === D.length - 1 ? 0.7 : null)
+            .text(d => d);
     }
   }
 }
